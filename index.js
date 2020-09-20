e5bf69d1e5b9243a10b765ae634358b84a030648
Kristof Dewilde <git@krivi.be>
2020-09-20 19:25:25 +0200

    Good commit

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+require('dotenv').config()
+
 const Discord = require('discord.js')
 
-require('dotenv').config()
+const prefixes = [
+    'Heads up!',
+    'Ding!',
+    'PSA!'
+]
+const prefix = () => prefixes[Math.floor(Math.random() * prefixes.length)]
+
+const gameQuotes = [
+    'Why not you team up?',
+    'GLHF!',
+    'Only Ws today.'
+]
+const gameQuote = () => gameQuotes[Math.floor(Math.random() * gameQuotes.length)]
+
+const voiceQuotes = [
+    'Such a sweet voice!',
+    'Go say hi!',
+    'Join for good vibes.'
+]
+const voiceQuote = () => voiceQuotes[Math.floor(Math.random() * voiceQuotes.length)]
 
 const client = new Discord.Client()
 client.once('ready', () => {
@@ -14,26 +35,32 @@ client.login(process.env.DISCORD_TOKEN)
     .then(channel => {
         client.on('voiceStateUpdate', (oldState, newState) => {
             if (!oldState.channelID && newState.channelID) {
-                console.log(`${newState.member.id} joined the ${newState.channel.name} voice channel!`)
-                //channel.send(`<@${newState.member.id}> joined the ${newState.channel.name} voice channel!`)
+                console.log(`${prefix()} <@${newState.member.id}> joined the **${newState.channel.name}** voice channel! ${voiceQuote()}`)
+                channel.send(`${prefix()} <@${newState.member.id}> joined the **${newState.channel.name}** voice channel! ${voiceQuote()}`)
             } else if (oldState.channelID && !newState.channelID) {
                 console.log(`${oldState.member.displayName} left the ${oldState.channel.name} voice channel!`)
-            } else if (oldState.channelID && newState.channelID) {
-                console.log(`${oldState.member.displayName} left the ${oldState.channel.name} voice channel to join ${newState.channel.name} instead!`)
+            } else if (oldState.channelID && newState.channelID && oldState.channelID !== newState.channelID) {
+                console.log(`${prefix()} <@${newState.member.id}> switched to the **${newState.channel.name}** voice channel! ${voiceQuote()}`)
+                channel.send(`${prefix()} <@${newState.member.id}> switched to the **${newState.channel.name}** voice channel! ${voiceQuote()}`)
+            } else if (oldState.channelID === newState.channelID && !oldState.streaming && newState.streaming) {
+                console.log(`${prefix()} <@${newState.member.id}> started streaming in the **${newState.channel.name}** voice channel! ${voiceQuote()}`)
+                channel.send(`${prefix()} <@${newState.member.id}> started streaming in the **${newState.channel.name}** voice channel! ${voiceQuote()}`)
             } else {
                 console.log('That was something else...')
+                // console.log(oldState)
+                // console.log(newState)
             }
         })
 
         client.on('presenceUpdate', (oldPresence, newPresence) => {
             const newGame = newPresence.activities.find(activity => activity.type === 'PLAYING')
 
-            if (newPresence.member.user.bot || !newGame) return;
+            if (newPresence.member.user.bot || !newGame) return
 
             const oldGame = oldPresence.activities.find(activity => activity.type === 'PLAYING')
 
             if (newGame !== oldGame) {
-                console.log(`${newPresence.userID} started playing ${newGame.name}!`)
+                channel.send(`${prefix()} <@${newPresence.member.id}> started playing **${newGame.name}**! ${gameQuote()}`)
             }
         })
     })
