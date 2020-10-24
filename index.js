f6895814dc8a0cf65b062aa5349533238ef40c7c
Kristof Dewilde <git@krivi.be>
2020-10-24 18:31:26 +0200

    Added role mentioning

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const margin = 30 * 60000 // time to remember games in ms
 const memory = {}
 const blacklist = ['BattleEye Launcher']
 
+const gameRoles = {
+    game356877880938070016: '695291606093267054', // Steam Rocket League
+    game11111111111: '695290745216565308' // Fortnite
+}
+
 const prefixes = [
     'Heads up!',
     'Ding!',
@@ -44,15 +49,15 @@ client.login(process.env.DISCORD_TOKEN)
     .then(channel => {
         client.on('voiceStateUpdate', (oldState, newState) => {
             if (!oldState.channelID && newState.channelID) {
-                console.log(`${prefix()} <@${newState.member.displayName}> joined the **${newState.channel.name}** voice channel! ${voiceQuote()}`)
+                console.log(`${prefix()} ${newState.member.displayName} joined the ${newState.channel.name} voice channel! ${voiceQuote()}`)
                 channel.send(`${prefix()} <@${newState.member.id}> joined the **${newState.channel.name}** voice channel! ${voiceQuote()}`)
             } else if (oldState.channelID && !newState.channelID) {
                 console.log(`${oldState.member.displayName} left the ${oldState.channel.name} voice channel!`)
             } else if (oldState.channelID && newState.channelID && oldState.channelID !== newState.channelID) {
-                console.log(`${prefix()} <@${newState.member.displayName}> switched to the **${newState.channel.name}** voice channel! ${voiceQuote()}`)
+                console.log(`${prefix()} ${newState.member.displayName} switched to the **${newState.channel.name}** voice channel! ${voiceQuote()}`)
                 channel.send(`${prefix()} <@${newState.member.id}> switched to the **${newState.channel.name}** voice channel! ${voiceQuote()}`)
             } else if (oldState.channelID === newState.channelID && !oldState.streaming && newState.streaming) {
-                console.log(`${prefix()} <@${newState.member.displayName}> started streaming in the **${newState.channel.name}** voice channel!`)
+                console.log(`${prefix()} ${newState.member.displayName} started streaming in the **${newState.channel.name}** voice channel!`)
                 channel.send(`${prefix()} <@${newState.member.id}> started streaming in the **${newState.channel.name}** voice channel!`)
             } else {
                 console.log('That was something else...')
@@ -67,31 +72,29 @@ client.login(process.env.DISCORD_TOKEN)
             const activity = newPresence.activities.find(activity => activity.type === 'PLAYING')
             if (!activity) return // activity was LISTENING or something else
 
-            console.log('# new PLAYING activity:')
-            console.log(activity)
+            console.log('# new PLAYING activity:', activity.applicationID)
+            // console.log(activity)
 
-            const oldMemory = memory['user' + newPresence.member.id]
-            memory['user' + newPresence.member.id] = {
+            const oldMemory = memory[`user${newPresence.member.id}`]
+            memory[`user${newPresence.member.id}`] = {
                 game: activity && activity.name ? activity.name.trim() : 'not playing anymore',
                 date: new Date()
             }
 
-            console.log('# old memory:')
-            console.log(oldMemory)
-            console.log('# new memory:')
-            console.log(memory['user' + newPresence.member.id])
+            console.log('old memory:', oldMemory)
+            console.log('new memory:', memory[`user${newPresence.member.id}`])
 
             if (!activity.name || blacklist.includes(activity.name.trim())) {
-                console.log('# not playing anymore or playing blacklisted name! Aborting...')
-                return
-            }
-
-            if (!oldMemory // User just started playing a new game
+                console.log(member.displayName, 'is not playing anymore or playing blacklisted name!')
+            } else if (!oldMemory // User just started playing a new game
                 || oldMemory.game.toUpperCase() !== activity.name.trim().toUpperCase() // User started playing a different game than last time
                 || new Date(oldMemory.date.getTime() + margin) < new Date()) { // User might have started playing the same game, but enough time has passed
-                console.log(`${prefix()} <@${newPresence.member.displayName}> started playing **${activity.name.trim()}**! ${gameQuote()}`)
-                channel.send(`${prefix()} <@${newPresence.member.id}> started playing **${activity.name.trim()}**! ${gameQuote()}`)
+                const gameRole = gameRoles[`game${activity.applicationID}`]
+                const game = gameRole ? `<@&${gameRole}>` : `**${activity.name.trim()}**`
+                console.log(`${prefix()} ${newPresence.member.displayName} started playing ${activity.name.trim()}! ${gameQuote()}`)
+                channel.send(`${prefix()} <@${newPresence.member.id}> started playing ${game}! ${gameQuote()}`)
+            } else {
+                console.log(`${newPresence.member.displayName} is still playing the same game.`)
             }
-            // if we get here the user must still be playing the same game
         })
     })
